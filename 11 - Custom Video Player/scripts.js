2622401474b6d490ad8bba9cc0d83e380f2862cc
Yuping Kuei <kueiyuping@gmail.com>
2021-02-17 13:37:25 +0900

    learnt how to manipulate the video properties

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,54 @@
+// get elements
+const player = document.querySelector(".player");
+const video = player.querySelector(".player__video");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+// functions
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  console.log(e.offsetX);
+  const scrubTime = (e.offsetX / video.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+// event listeners
+video.addEventListener("click", togglePlay);
+video.addEventListener("click", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mousemove", () => (mousedown = false));
